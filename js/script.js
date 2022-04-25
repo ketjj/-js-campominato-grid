9b5dffc509fe47e59e3efd1a435589f9cae3b959
ketjj <jorjolianiket@gmail.com>
2022-04-25 18:28:12 +0200

    creating main structure with grid

--- js/script.js
+++ js/script.js
@@ -4,53 +4,89 @@
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+
+const bombNumbers = 15;
+
 const playButton = document.getElementById('btn-output').addEventListener('click', init);
 
-//console.log(container);
 
-//init(container);
+const select = document.getElementById('inputGroupSelect04');
+const value = select.options[select.selectedIndex].value;
+
 
 function init(){
 
-const container = document.querySelector('.main_container');
+  const container = document.querySelector('.main_container');
 
-// svuota container di tutte le celle
-while (container.firstChild) {
+  // svuota container di tutte le celle
+  while (container.firstChild) {
   container.removeChild(container.lastChild);
+  }
+
+  create(container, parseInt(value));
+  const bombs = createBomb();
+
 }
 
-const select = document.getElementById('inputGroupSelect04');
-let value = select.options[select.selectedIndex].value;
-console.log(value); 
-create(container, parseInt(value))
-}
- 
-function create(element, numLoops){
-  console.log("create");
-  for(let i = 0; i < numLoops; i++){
-    createBox(element, numLoops, i+1)
+function create(element, numLoops, num){
+
+  for(let i = 1; i <= numLoops; i++){
+    createBox(element, numLoops,num)
   }
 }
 
 function createBox(element1, numLoops1, num) {
- 
+  
   const box = document.createElement('div');
+  
   box.addEventListener('click', function changeColor(){
     box.classList.add('box-clicked');
   })
+
+  num = getRandomNumber(1, numLoops1);
   box.innerHTML = num;
+  
   if( numLoops1 === 100)
-       box.className = 'box-easy';
+  box.className = 'box-easy';
   else if( numLoops1 === 81)
-       box.className = 'box-hard';
+  box.className = 'box-hard';
   else if(numLoops1 === 49)
-       box.className = 'box-veryhard'; 
+  box.className = 'box-veryhard'; 
   element1.append(box);
 }
 
+// function createUniqueRandomNumbers(){
+//   let randomNumber;
+//   const maxLimit = value;
+//   const minLimit = 1;
+//   const uniqueRandomNumbers = [];
+
+//   while (uniqueRandomNumbers.length < maxLimit) {
+//     const randomNumbers = getRandomNumber(minLimit, maxLimit)
+//     if (uniqueRandomNumbers.indexOf(randomNumbers) == -1) { 
+       
+//       uniqueRandomNumbers.push( randomNumbers );
+//     }
+// }
+//   return randomNumber;
+// }
 
 
+function createBomb(){
+  const generateBombs = [];
+  
+  while(generateBombs.length < bombNumbers){
+    const bomb = getRandomNumber(1, bombNumbers);
+    
+    if(!generateBombs.includes(bomb)){
+      generateBombs.push(bomb)
+    }
+  }
+  return generateBombs;
+}
 
+// console.log(createBomb());
 
-
-
+function getRandomNumber(min,max){
+  return Math.floor(Math.random()* (max - min + 1) + min);
+}
